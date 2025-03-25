18531160033194ab211f7c9df9ce04ee963bf5a3
ST10486848 <ST10486848@vcconnect.edu.za>
2025-03-25 14:37:43 +0200

    Initial commit

---
+++ app/src/test/java/za/co/varsitycollege/st10486848/st10486848_project1/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package za.co.varsitycollege.st10486848.st10486848_project1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
